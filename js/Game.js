b01035addc696ea3df1b75e10b13a53d82a68a12
Michael Kilcorse <kilcorse.michael92@gmail.com>
2019-03-04 07:20:05 -0500

    Initial commit

---
+++ js/Game.js
@@ -0,0 +1,60 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+ class Game (){
+   constructor(){
+       this.missed = 0;
+       this.phrases = [];
+       this.activePhrase = null;
+    }
+    /**
+    * randomly retrieves one of the phrases stored in the phrases array
+    * @returns {index} returns one of the phrases in the phrases array
+    */
+
+    getRandomPhrase(){
+
+    }
+    /**
+    * checks to see if the button clicked by the player matches a letter in the phrase
+    *
+    */
+
+    handleInteraction(){
+
+    }
+    /**
+    * Removes 1 life from players scorboard
+    *
+    */
+
+    removeLife(){
+
+    }
+
+    /**
+    * Initializes the game
+    *
+    */
+
+    startGame(){
+
+    }
+    /**
+    * Checks to see if player has revealed all of the letters of the active phrase
+    *
+    */
+
+    checkForWin(){
+
+    }
+    /**
+    * Displays the original start screen and updates the h1 element with a win or loss message
+    *
+    */
+
+    gameOver(){
+      
+    }
+ }
---
+++ js/Phrase.js
@@ -0,0 +1,34 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
+
+ class Phrase(){
+   constructor(phrase){
+     this.phrase = phrase
+
+   }
+   /**
+   * Adds letter placeholders to display when game starts
+   *
+   */
+
+   addPhraseToDisplay(){
+
+   }
+   /**
+   * checks to see if the letter selected matches a letter in the phrase
+   *
+   */
+
+   checkLetter(){
+
+   }
+   /**
+   * reveals the letter(s) on the board that matches the players selection
+   *
+   */
+
+   showMatchedLetter(){
+     
+   }
+ }
